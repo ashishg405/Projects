d522f60cc62a24f433ddb51b6d7c7e1a1ba0b1d2
ashishg405 <ashishg405@gmail.com>
2021-07-31 22:37:44 +0530

    Updated file watcher

--- File_Watcher/File_Watcher/FileWatcher.h
+++ File_Watcher/File_Watcher/FileWatcher.h
@@ -4,15 +4,16 @@
 #include <thread>
 #include <chrono>
 #include <map>
+#include <functional>
 
 class FileWatcher
 {
 private:
 	std::map<std::string, std::thread> exprs;
 	std::filesystem::path path_to_watch;
-	void _register_expr(std::regex expr, void callback(std::filesystem::path));
+	void _register_expr(std::regex expr, std::function<void(std::filesystem::path)> callback);
 public:
 	FileWatcher(std::filesystem::path watch_path);
-	void register_expr(std::string expr, void callback(std::filesystem::path));
+	void register_expr(std::string expr, std::function<void(std::filesystem::path)> callback);
 };
 
